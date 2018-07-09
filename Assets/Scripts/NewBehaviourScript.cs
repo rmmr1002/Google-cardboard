72158e092a940b88ade241cdd30ac9b7cc215b78
Rohan <rmmr1002@gmail.com>
2018-07-09 15:36:39 -0400

    Master commit

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour {
+    public void Start()
+    {
+        XRSettings.enabled = false;
+    }
+
+
+	public void Changescene(string scenename)
+    {
+        XRSettings.enabled = true;
+        SceneManager.LoadScene(scenename);
+    }
+}
